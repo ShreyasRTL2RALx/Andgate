2c2d08f198635fd058880f9141183270778667f0
ShreyasRTL2RALx <shreyasmugalkhod3@gmail.com>
2025-10-18 12:50:49 +0530

    changes commited to old file

--- andgate.srcs/sources_1/new/andgate.v
+++ andgate.srcs/sources_1/new/andgate.v
@@ -1,29 +1,11 @@
-`timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 18.10.2025 12:20:50
-// Design Name: 
-// Module Name: andgate
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
+
 
 
 module andgate(
     input A,
     input B,
-    output Y
+    output Y,Z
     );
     assign Y=A&B;
+     assign Y=A^B;
 endmodule
